a2163e06944c80b7a403c13f2513f024f05919c6
Sarah Lewis <sarahlewis1808@gmail.com>
2023-03-31 10:04:56 +1300

    data files added with types

---
+++ data/balloonData.ts
@@ -0,0 +1,68 @@
+interface balloon {
+  id: number
+  img: string
+  isVisible: boolean
+}
+
+const balloons: balloon[] = [
+  {
+    id: 1,
+    img: '#',
+    isVisible: true,
+  },
+  {
+    id: 2,
+    img: '#',
+    isVisible: true,
+  },
+  {
+    id: 3,
+    img: '#',
+    isVisible: true,
+  },
+  {
+    id: 4,
+    img: '#',
+    isVisible: true,
+  },
+  {
+    id: 5,
+    img: '#',
+    isVisible: true,
+  },
+  {
+    id: 6,
+    img: '#',
+    isVisible: true,
+  },
+  {
+    id: 7,
+    img: '#',
+    isVisible: true,
+  },
+  {
+    id: 8,
+    img: '#',
+    isVisible: true,
+  },
+  {
+    id: 9,
+    img: '#',
+    isVisible: true,
+  },
+  {
+    id: 10,
+    img: '#',
+    isVisible: true,
+  },
+  {
+    id: 11,
+    img: '#',
+    isVisible: true,
+  },
+  {
+    id: 12,
+    img: '#',
+    isVisible: true,
+  },
+]
---
+++ data/prizeData.ts
@@ -0,0 +1,68 @@
+interface prize {
+  id: number
+  amount: number
+  isVisible: boolean
+}
+
+const prizes: prize[] = [
+  {
+    id: 1,
+    amount: 1,
+    isVisible: true,
+  },
+  {
+    id: 2,
+    amount: 5,
+    isVisible: true,
+  },
+  {
+    id: 3,
+    amount: 10,
+    isVisible: true,
+  },
+  {
+    id: 4,
+    amount: 20,
+    isVisible: true,
+  },
+  {
+    id: 5,
+    amount: 50,
+    isVisible: true,
+  },
+  {
+    id: 6,
+    amount: 100,
+    isVisible: true,
+  },
+  {
+    id: 7,
+    amount: 1000,
+    isVisible: true,
+  },
+  {
+    id: 8,
+    amount: 2000,
+    isVisible: true,
+  },
+  {
+    id: 9,
+    amount: 5000,
+    isVisible: true,
+  },
+  {
+    id: 10,
+    amount: 10000,
+    isVisible: true,
+  },
+  {
+    id: 11,
+    amount: 25000,
+    isVisible: true,
+  },
+  {
+    id: 12,
+    amount: 50000,
+    isVisible: true,
+  },
+]
